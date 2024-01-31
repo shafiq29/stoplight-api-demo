c91410ed6fca3216d585b93236ba1e196858ba3b
seed29bit@gmail.com <seed29bit@gmail.com>
2024-02-01 00:34:34 +0600

    Added .spectral.mjs

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjIzMzQwNg/spectral.js?branch_slug=master&token=458ad44a-1ea2-402e-8a81-a245cc668f4b";
+export default { extends: ruleset };
